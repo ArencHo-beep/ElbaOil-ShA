705ec1b9f913638677ae87718bb3f9404c6f51c7
Arenc <arenchoxha73@gmail.com>
2025-08-01 20:43:23 +0200

    added script with tailwind and initiated css styling

--- ElbaOil/Elba_Oil_app/admin.py
+++ ElbaOil/Elba_Oil_app/admin.py
@@ -3,3 +3,11 @@ from .models import Category, Product
 
 admin.site.register(Category)
 admin.site.register(Product)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
